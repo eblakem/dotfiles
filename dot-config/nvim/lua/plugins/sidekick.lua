4fa9afa0f8e3da7c22c6fbf1f99579628df5c016
michael <michael.blake@health.qld.gov.au>
2026-06-05 14:49:12 +1000

    wip

--- dot-config/nvim/lua/plugins/sidekick.lua
+++ dot-config/nvim/lua/plugins/sidekick.lua
@@ -2,9 +2,17 @@ vim.pack.add({
 	"https://github.com/folke/sidekick.nvim",
 }, { confirm = false })
 
-require("sidekick").setup({})
+require("sidekick").setup({
+	cli = {
+		tools = {
+			agy = {
+				cmd = { "agy" },
+			},
+		},
+	},
+})
 
 local vk = vim.keymap
 vk.set("n", "<leader>aa", function()
-	require("sidekick.cli").toggle({ name = "gemini", focus = true })
-end, { silent = true, desc = "Gemini toggle" })
+	require("sidekick.cli").toggle({ name = "agy", focus = true })
+end, { silent = true, desc = "Agy toggle" })
--- dot-config/nvim/lua/plugins/telescope.lua
+++ dot-config/nvim/lua/plugins/telescope.lua
@@ -4,64 +4,80 @@ vim.pack.add({
 	"https://github.com/nvim-telescope/telescope-file-browser.nvim",
 	"https://github.com/nvim-telescope/telescope.nvim",
 	"https://github.com/nvim-lua/plenary.nvim",
-}, { confirm = false })
+}, {
+	confirm = false,
+	callback = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+		local config = require("telescope.config")
 
-local telescope = require("telescope")
-local actions = require("telescope.actions")
-local config = require("telescope.config")
-
-local opts = {
-	defaults = {
-		mappings = {
-			i = {
-				["<Esc>"] = actions.close,
+		local opts = {
+			defaults = {
+				mappings = {
+					i = {
+						["<Esc>"] = actions.close,
+					},
+				},
+				-- Fallback pattern filtering
+				file_ignore_patterns = {
+					"bin/",
+					"obj/",
+					"%.git/",
+					"node_modules/",
+				},
 			},
-		},
-		file_ignore_patterns = {
-			"node_modules",
-		},
-	},
-	extensions = {
-		file_browser = {
-			hijack_netrw = true,
-			hidden = true,
-			no_ignore = true,
-		},
-	},
-}
+			extensions = {
+				file_browser = {
+					hijack_netrw = true,
+					hidden = true,
+					no_ignore = false, -- Changed to false to prevent showing ignored files here
+				},
+			},
+		}
 
-if vim.fn.executable("rg") == 1 then
-	local rg = { "rg", "--files", "--hidden", "--no-ignore", "--glob", "!**/.git/*" }
-	local vg_arg = { unpack(config.values.vimgrep_arguments) }
-	for i, rg_arg in ipairs(rg) do
-		if i > 2 then
-			table.insert(vg_arg, rg_arg)
+		if vim.fn.executable("rg") == 1 then
+			-- Clean commands that completely respect your .gitignore rules
+			local rg_find = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" }
+			local rg_grep = {
+				"rg",
+				"--color=never",
+				"--no-heading",
+				"--with-filename",
+				"--line-number",
+				"--column",
+				"--smart-case",
+				"--hidden",
+				"--glob",
+				"!**/.git/*",
+			}
+
+			local rg_scope = {
+				defaults = { vimgrep_arguments = rg_grep },
+				pickers = { find_files = { find_command = rg_find } },
+			}
+			opts = vim.tbl_deep_extend("force", opts, rg_scope)
 		end
-	end
-	local rg_scope = {
-		defaults = { vimgrep_arguments = vg_arg },
-		pickers = { find_files = { find_command = rg } },
-	}
-	opts = vim.tbl_deep_extend("force", opts, rg_scope)
-end
 
-telescope.setup(opts)
-telescope.load_extension("file_browser")
+		telescope.setup(opts)
+		telescope.load_extension("file_browser")
 
-local fzfutil = require("fzf-util")
-local fzfplugindir = vim.pack.get({ "telescope-fzf-native.nvim" })[1].path
-fzfutil.load(fzfplugindir)
+		local fzfutil = require("fzf-util")
+		local fzfplugindir = vim.pack.get({ "telescope-fzf-native.nvim" })[1].path
+		fzfutil.load(fzfplugindir)
 
-vim.api.nvim_create_autocmd("PackChanged", {
-	callback = function(packOpts)
-		if packOpts.data.spec.name == "telescope-fzf-native.nvim" and packOpts.data.kind == "update" then
-			fzfutil.build(fzfplugindir, function()
-				telescope.load_extension("fzf")
-			end)
-		end
+		vim.api.nvim_create_autocmd("PackChanged", {
+			callback = function(packOpts)
+				if packOpts.data.spec.name == "telescope-fzf-native.nvim" and packOpts.data.kind == "update" then
+					fzfutil.build(fzfplugindir, function()
+						telescope.load_extension("fzf")
+					end)
+				end
+			end,
+		})
 	end,
 })
 
+-- Keymaps
 local vk = vim.keymap
 vk.set("n", "<leader>ff", function()
 	require("telescope.builtin").find_files({ hidden = false })
